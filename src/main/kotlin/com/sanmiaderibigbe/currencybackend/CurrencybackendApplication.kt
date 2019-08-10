a6c0a3db3129467ca132490cff156ef0462eea9b
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-08-10 18:05:36 +0100

    intial commit.

---
+++ src/main/kotlin/com/sanmiaderibigbe/currencybackend/CurrencybackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sanmiaderibigbe.currencybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencybackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CurrencybackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/sanmiaderibigbe/currencybackend/CurrencybackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sanmiaderibigbe.currencybackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CurrencybackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
